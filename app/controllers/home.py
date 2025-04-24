c08fdd4272a536db3d1f23a6cae9adf8f35a5e91
Aaron Santana <santanavaldelomaraa@gmail.com>
2025-04-24 07:02:30 -0500

    Implement audit logging for home page view

--- app/controllers/home.py
+++ app/controllers/home.py
@@ -1,8 +1,9 @@
 from flask import Blueprint, render_template
-
+from app.services.audit import log_audit, AuditActionType
 home = Blueprint('home', __name__)
 
 @home.route("/")
 @home.route('/<name>')
 def greeting(name=None):
+    log_audit("Aaron", AuditActionType.CONTENT_READ, "Home page viewed")
     return render_template('home.html', person=name)
