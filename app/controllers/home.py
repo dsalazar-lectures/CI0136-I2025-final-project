9d4b8475a6afe438b5fcf8840add86ac1ebc7278
kevin.ramirezbarboza <kevin.ramirezbarboza>
2025-04-27 14:00:10 -0600

    Desarrollo de la funcionalidad del review (utilizando mocks)

--- app/controllers/home.py
+++
@@ -1,8 +0,0 @@
-from flask import Blueprint, render_template
-
-home = Blueprint('home', __name__)
-
-@home.route("/")
-@home.route('/<name>')
-def greeting(name=None):
-    return render_template('home.html', person=name)
