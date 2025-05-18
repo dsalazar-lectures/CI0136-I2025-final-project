7143456002b85a0a65ecd87d83ae3cb3fe3a5615
Werner Naranjo Navarro <werner.naranjo@ucr.ac.cr>
2025-05-18 16:09:06 -0600

    Create tutorial simple html file

--- app/__init__.py
+++ app/__init__.py
@@ -19,7 +19,7 @@ initialize_firebase()
 from .controllers.home import home_bp as home_blueprint
 from .controllers.register import register_bp as register_blueprint  
 from .controllers.auth import auth_bp as auth_blueprint
-from .controllers.tutoriaControllers import tutoring as tutoria_blueprint
+from .controllers.tutoriaControllers import tutoring as tutorial_blueprint
 from .controllers.comments import comments_bp
 from .controllers.ratings import ratings_bp
 from .controllers.email_controller import mail_bp
@@ -31,7 +31,7 @@ from .controllers.student_profile import student_bp
 app.register_blueprint(home_blueprint)          # Home page routes
 app.register_blueprint(register_blueprint)      # Registration routes
 app.register_blueprint(auth_blueprint)          # Authentication routes
-app.register_blueprint(tutoria_blueprint)      # Tutoring routes
+app.register_blueprint(tutorial_blueprint)      # Tutoring routes
 app.register_blueprint(comments_bp)
 app.register_blueprint(ratings_bp, url_prefix='/comments')
 app.register_blueprint(mail_bp, url_prefix='/email')
