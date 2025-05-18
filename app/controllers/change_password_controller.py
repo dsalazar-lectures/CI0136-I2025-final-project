2588103435b4985b897823f4cc4d7e5be9531d78
isaaczzs <isacczunigaserrano@gmail.com>
2025-05-18 09:42:09 -0600

    adding password service unit tests

--- app/controllers/change_password_controller.py
+++ app/controllers/change_password_controller.py
@@ -3,8 +3,6 @@ from app.models.services.email_service import EmailService
 from app.models.builders.email_notification_builders import Builder
 from app.models.services import email_service
 from app.models.services import password_service
-from app.models.builders import body_factories
-from app.models.builders.body_factories import BodyFactory
 from app.models.services import email_notifier
 
 
@@ -27,24 +25,23 @@ def change_pass():
      flash("Las contraseñas no coinciden", "error")
      return redirect("/change_password")
     else:
-        factory = body_factories.EmailBuilderFactory()
         service = password_service.ChangePasswordService()
         notifier = email_notifier.SMTPNotifier()
-        #Pepito since there's still no user, a valid logged in user must be passed through the method
         valid_password = service.validate_password(new_password_input)
+        #Pepito since there's still no user, a valid logged in user must be passed through the method
         if (valid_password and service.change_password(current_password_input, new_password_input, "Pepito Salazar")):
             flash("Contraseña cambiada exitosamente", "success")
 
             '''
             #LOGIC TO SEND EMAIL AFTER CHANGING THE PASSWORD. USER SESSION REQUIRED
-            if (notifier.send(factory, user, "changepassword")):
+            if (notifier.send(user, "changepassword")):
                #el correo se envio exitosamente
             else:
                #el correo no se envio exitosamente
             '''   
         else: 
             if (valid_password):
-                flash("La contraseña ingresada no es valida", "error") #the current password specified is not the one stored in the DB
+                flash("La actual contraseña ingresada no es valida", "error") #the current password specified is not the one stored in the DB
             else: 
                flash("La contraseña debe tener 8 caracteres, mayuscula, minuscula y simbolos", "error") #the password doesn't meet the standars
         return redirect("/change_password")
--- app/models/services/email_notifier.py
+++ app/models/services/email_notifier.py
@@ -4,16 +4,18 @@ from app.models.builders.body_factories import BodyFactory
 from app.models.services.email_service import EmailService
 from app.models.services import email_service
 from app.models.builders.email_notification_builders import Builder
+from app.models.builders import body_factories
 
 class EmailNotifier(ABC):
 
     @abstractmethod
-    def send(self, factory: BodyFactory, user):
+    def send(self, user, builder_type: str):
         pass
 
 
 class SMTPNotifier(EmailNotifier):
-    def send(self, factory: BodyFactory, user, builder_type: str):
+    def send(self, user, builder_type: str):
+        factory = body_factories.EmailBuilderFactory()
         smtp_service = email_service.SMTPEmailService()
         builder = factory.createBody(builder_type)
         notification_data = builder.buildBody()
