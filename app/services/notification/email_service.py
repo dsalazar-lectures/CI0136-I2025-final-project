f1f522d39c69a9282401e437150bcb28fa14d193
Angel Mena Coudin <kratosmorze@gmail.com>
2025-05-22 10:26:02 -0600

    Boceto clase EmailService

--- app/services/notification/email_service.py
+++ app/services/notification/email_service.py
@@ -1,2 +1,15 @@
-class service():
+from app.services.notification.smtp_email_type import SMTPEmailService
+from app.services.notification.factories.email_factory import EmailBuilderFactory
+
+class Emailservice():
+    def __init__(self):
+        self.factory = EmailBuilderFactory()
+        self.smtp_service = SMTPEmailService()
     
+    def send_email(self, builder_type: str, data: dict):
+        # Create the email body using the factory
+        builder = self.factory.createBody(builder_type)
+        email_body = builder.buildBody(data)
+        
+        # Send the email using the SMTP service
+        self.smtp_service.send(email_body)
