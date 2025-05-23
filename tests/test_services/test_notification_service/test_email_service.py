72c1bc313a1477173a76a28a944e2d1113ed8059
JJ <139706657+ViqJ@users.noreply.github.com>
2025-05-23 02:32:02 +0000

    Test email service

--- tests/test_services/test_notification_service/test_email_service.py
+++ tests/test_services/test_notification_service/test_email_service.py
@@ -1,14 +1,20 @@
 import pytest
 
-from app.services.notification import Emailservice
-
-@pytest.fixture
-def email_service():
-    return Emailservice()
+from app.services.notification import EmailService
+from app.services.notification import EmailBuilderFactory
+from app.services.notification import SMTPEmailService
 
 
-def test_create_email_service(email_service):
+
+def test_create_email_service():
+    factory = EmailBuilderFactory()
+    smtp_service = SMTPEmailService()
+    email_service = EmailService(factory, smtp_service)
     assert email_service is not None
-    assert isinstance(email_service, Emailservice)
+    assert isinstance(email_service, EmailService)
 
-    
+def test_send_email_correct(email_service):
+    result = email_service.send_email('login', {'username':'JJ','emailTo': '123@123.com'})
+    assert result is True
+
+
