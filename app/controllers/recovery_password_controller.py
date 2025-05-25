faa8a5e91219ebe4a5e60929fa81e10e5b5aa6ab
Angel Mena Coudin <kratosmorze@gmail.com>
2025-05-24 22:09:12 -0600

    Add: lógica de recuperación de contraseña

--- app/controllers/recovery_password_controller.py
+++ app/controllers/recovery_password_controller.py
@@ -4,6 +4,9 @@ from app.models.repositories.users.firebase_user_repository import FirebaseUserR
 # Para crear token firmado
 from itsdangerous import URLSafeTimedSerializer
 # Para enviar correo
+from app.services.notification import send_email_notification
+# Para logs
+from app.services.audit import log_audit, AuditActionType
 
 repo = FirebaseUserRepository()
 
@@ -22,10 +25,34 @@ def recover():
         if user:
             token = generate_password_reset_token(userEmail)
             reset_link = url_for('recoveryPassword.reset_password_form', token=token, _external=True)
-            flash("Recovery email sent successfully.", "success")
-            return render_template("recovery_pass_token.html")
+            
+            # Prepare email data for notification
+            email_data = {
+                "username": user.get("name", "Usuario"),
+                "emailTo": userEmail,
+                "reset_link": reset_link
+            }
+
+            # Send the email notification
+            if not send_email_notification("recoveryPassword", email_data):
+                log_audit(
+                    user = user.get("name", "Usuario"),
+                    action_type = AuditActionType.USER_PASSWORD_RESET,
+                    details = "Fallo al enviar el correo de recuperación de contraseña",
+                )
+                flash("Error al enviar correo, por favor, intenta de nuevo.", "danger")
+                return redirect(url_for("recoveryPassword.recoveryPasswordView"))
+            
+            log_audit(
+                user = user.get("name", "Usuario"),
+                action_type = AuditActionType.USER_PASSWORD_RESET,
+                details = "Correo de recuperación de contraseña enviado",
+                )
+            
+            flash("Correo de recuperación enviado correctamente, por favor revisa tu correo.", "success")
+            return redirect(url_for("recoveryPassword.recoveryPasswordView"))
         else:
-            flash("Please enter a valid email address.", "danger")
+            flash("Ingrese una dirección de correo válida.", "danger")
             return redirect(url_for("recoveryPassword.recoveryPasswordView"))
     else:
         flash("Please enter a valid email address.", "danger")
@@ -33,12 +60,12 @@ def recover():
     return redirect(url_for("recoveryPassword.recoveryPasswordView"))
 
 def generate_password_reset_token(email):
-    serializer = URLSafeTimedSerializer(current_app.config['secret_key'])
+    serializer = URLSafeTimedSerializer(current_app.secret_key)
     return serializer.dumps(email, salt='password-recovery-salt')
 
 @rec_password_bp.route("/reset_password/<token>", methods=['GET', 'POST'])
 def reset_password_form(token):
-    serializer = URLSafeTimedSerializer(current_app.config['secret_key'])
+    serializer = URLSafeTimedSerializer(current_app.secret_key)
 
     try:
         email = serializer.loads(token, salt='password-recovery-salt', max_age=900)  # 15 minutos
@@ -48,8 +75,15 @@ def reset_password_form(token):
 
     if request.method == 'POST':
         new_password = request.form.get('password')
-        FirebaseUserRepository.update_user_password(email, new_password)
-        flash("Password reset successfully.", "success")
-        return redirect(url_for("login"))
+        confirm_password = request.form.get('confirm_password')
+        if new_password != confirm_password:
+            flash("Las contraseñas no coinciden.", "danger")
+            return redirect(url_for("recoveryPassword.reset_password_form", token=token))
+        
+        # Actualizar la contraseña en Firebase
+        repo.update_user_password(email, new_password)
+        
+        flash("Contraseña actualizada correctamente.", "success")
+        return redirect(url_for("auth.login"))
 
     return render_template("reset_password.html", token=token)
