91e590196892ed0e5dda7b162f0e1249a738095a
GYPSI.PARAMO <gipsyparamolopez@gmail.com>
2025-06-15 15:34:27 -0600

    fix: update google authentication tests

--- tests/test_google_auth.py
+++ tests/test_google_auth.py
@@ -1,14 +1,14 @@
 import pytest
 from flask import session
 from unittest.mock import patch, MagicMock
-from app import app 
+from app import app
 
 @pytest.fixture
 def client():
     app.config["TESTING"] = True
     with app.test_client() as client:
         yield client
-# user registered
+
 @patch("app.controllers.auth.firebase_auth.verify_id_token")
 @patch("app.controllers.auth.FirebaseUserRepository")
 def test_google_login_existing_user(mock_repo_class, mock_verify_token, client):
@@ -21,18 +21,20 @@ def test_google_login_existing_user(mock_repo_class, mock_verify_token, client):
     mock_repo = MagicMock()
     mock_repo.get_user_by_email.return_value = {
         "id": 1,
-        "role": "student",
+        "role": "Student",
         "email": "test@example.com",
-        "name": "Test User"
+        "name": "Test User",
+        "status": "active"
     }
     mock_repo_class.return_value = mock_repo
 
     response = client.post("/auth/google-login", json={"token": "fake-token"})
 
     assert response.status_code == 200
-    assert response.json["message"] == "Login with Google successful"
+    assert response.json["message"] == "Login con Google exitoso"
 
-# new user
+
+
 @patch("app.controllers.auth.firebase_auth.verify_id_token")
 @patch("app.controllers.auth.FirebaseUserRepository")
 def test_google_login_new_user(mock_repo_class, mock_verify_token, client):
@@ -43,7 +45,17 @@ def test_google_login_new_user(mock_repo_class, mock_verify_token, client):
     }
 
     mock_repo = MagicMock()
-    mock_repo.get_user_by_email.return_value = None 
+    mock_repo.get_user_by_email.return_value = None
+
+
+    mock_repo.add_user.return_value = {
+        "id": 2,
+        "role": "Student",
+        "email": "newuser@example.com",
+        "name": "New User",
+        "status": "pending"
+    }
+
     mock_repo_class.return_value = mock_repo
 
     response = client.post("/auth/google-login", json={"token": "fake-token"})
@@ -52,8 +64,9 @@ def test_google_login_new_user(mock_repo_class, mock_verify_token, client):
         name="New User",
         email="newuser@example.com",
         password=None,
-        role="student"
+        role="Student"
     )
 
     assert response.status_code == 200
-    assert response.json["message"] == "Login with Google successful"
+    assert response.json["message"] == "Login con Google exitoso"
+
